a3693598746b3b1907e6952d70133a8a521d979f
jeanmichelneto <jeanmichelneto@gmail.com>
2022-03-28 11:32:41 -0300

    course python pmp

---
+++ aula_15.py
@@ -0,0 +1,15 @@
+# def bom_dia():
+# print('Bom Dia!')
+#  print('Bom Dia!')
+# print('Bom Dia!')
+
+# bom_dia()
+
+def chamada():
+    for x in lista_espera:
+        print('O Nome a ser chamado é ', x)
+
+
+lista_espera = ['Michel', 'Pedro', 'Maria Aparecida']
+
+chamada()
---
+++ aula_16.py
@@ -0,0 +1,4 @@
+clientes = ['José', 'Maria', 'Bastiana']
+
+for nome in clientes:
+    print('Olá', nome, 'Seu pedido foi enviado com sucesso!')
